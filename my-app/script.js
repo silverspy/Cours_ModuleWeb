42036ace2ceb290907570e42526bd828661f1b8b
Gilles <silverspy@gmx.com>
2020-03-20 15:20:33 +0100

    Test et MiniTP

---
+++ my-app/script.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+'use strict';
+var file = process.argv[2];
+
+if(!file){
+    console.error('Missing argument. Please give file name. Example node script.js test.csv');
+    process.exit(1)
+}
+
+fs.readFile(file,'utf8', (err,data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    console.log(data)
+})
