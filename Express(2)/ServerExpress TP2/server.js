4f6c352f1b84c7f3c354a847c85ff196ee5a4e5d
Gilles <silverspy@gmx.com>
2020-03-26 18:46:40 +0100

    Fin TP2 Express

--- Express(2)/ServerExpress TP2/server.js
+++ Express(2)/ServerExpress TP2/server.js
@@ -1,76 +1,112 @@
-const fs = require('fs');
+const fs = require('fs')
 const express = require('express');
 const path = require('path');
-var file = process.argv[2];
-var content;
-const app = express();
+const bodyParser = require('body-parser');
+const {v4: uuidv4} = require('uuid');
 
-if(!file){
-    console.error('Missing argument. Please give file name. Example node script.js test.csv');
-    process.exit(1)
-}
+
+const app = express();
 
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res) {
-    fs.readFile(file,'utf8', (err,data) => {
-        if (err) {
-            console.error(err)
-            res.status(500).send('Sorry, no files found')
-            process.exit(1)
-        }
-
-        var results = data.toString().split(/\r\n|\n/)
-        var tab = '<table>';
-        
-        for (let result of results) {
-            user = result.split(';');
-            tab += `<tr><td>${user[0]}</td><td>${user[1]}</td></tr>`
-        }
-        
-        tab += '<style type="text/css">td { border-bottom: 1px solid #ddd; }</style>'
-        tab += '</table>';
-
-        res.status(200).send(tab)
-
-        
-    })
-
-});
-
-app.listen(port, () => console.log("ok"));
-
-/*
-const server = http.createServer(
-    (req, res) => {
-        const generatedTemplate = compiledFunction({
-            name: 'test'
-        })
-
-        fs.readFile(file,'utf8', (err,data) => {
+app.get('/cities', (req, res) => {
+    if (!fs.existsSync(path.join(__dirname, 'cities.json'))) {
+        res.status(404).send('Sorry, no files found')
+    } else {
+        fs.readFile(path.join(__dirname, 'cities.json'), 'utf-8', (err, data) => {
             if (err) {
-                console.error(err)
-                process.exit(1)
+                res.status(500).send('Error')
+                console.log(err)
+            } else {
+                res.send(data)
             }
-            var results = data.toString().split(/\r\n|\n/)
-            var tab = '<table>';
-            for (let result of results) {
-                user = result.split(';');
-                tab += `<tr><td>${user[0]}</td><td>${user[1]}</td></tr>`
-            }
-            tab += '<style type="text/css">td { border-bottom: 1px solid #ddd; }</style>'
-            tab += '</table>';
-
-            
-        res.statusCode = 200;
-        res.setHeader('Content-Type','text/html')
-        res.end(generatedTemplate + tab)
         })
     }
-)
+})
 
-server.listen(port, () => {
-    console.log('Server running at port' + port)
-}) */
+app.post('/city', bodyParser.json(), (req, res, next) => {
+    const city = req.body
+    if (!fs.existsSync(path.join(__dirname, 'cities.json'))) {
+        const object = {}
+        object.cities = []
+        let json = JSON.stringify(object)
+        fs.writeFileSync(path.join(__dirname, 'cities.json'), json)
+    }
+
+    fs.readFile(path.join(__dirname, 'cities.json'), (err, data) => {
+        if (err) {
+            res.status(500).send('Error');
+            console.log(err);
+        } else {
+            const object = JSON.parse(data);
+            if (object.cities.some(c => c.name === city.name)) {
+                res.status(500).send('City already exist');
+            } else {
+                object.cities.push({
+                    id: uuidv4(),
+                    name: city.name
+                });
+                const json = JSON.stringify(object);
+                fs.writeFileSync(path.join(__dirname, 'cities.json'), json);
+                res.send(json);
+            }
+        }
+    })
+
+})
+
+app.put('/city/:id', bodyParser.json(), (req, res) => {
+    const city = req.body
+    if (fs.existsSync(path.join(__dirname, 'cities.json'))) {
+        fs.readFile(path.join(__dirname, 'cities.json'), (err, data) => {
+            if (err) {
+                res.status(500).send('Error');
+                console.log(err);
+            } else {
+                const files = JSON.parse(data)
+                const index = files.cities.findIndex(c => c.id === city.id)
+                if (index === -1) {
+                    res.sendStatus(404)
+                } else {
+                    files.cities[index].name = city.name
+                    const json = JSON.stringify(files)
+                    fs.writeFileSync(path.join(__dirname, 'cities.json'), json)
+                    res.send(json)
+                }
+            }
+        })
+    } else {
+        console.log('File do not exists')
+        res.status(404).send('No file');
+    }
+})
+
+app.delete('/city/:id', bodyParser.json(), (req, res) => {
+    const id = req.params.id
+    if (fs.existsSync(path.join(__dirname, 'cities.json'))) {
+        fs.readFile(path.join(__dirname, 'cities.json'), (err, data) => {
+            if (err) {
+                res.status(500).send('Error');
+                console.log(err);
+            } else {
+                const files = JSON.parse(data)
+                const index = files.cities.findIndex(city => city.id === id)
+                if (index === -1) {
+                    res.status(500).send('No city');
+                } else {
+                    files.cities = files.cities.filter(c => c.id !== id)
+                    const json = JSON.stringify(files)
+                    fs.writeFileSync(path.join(__dirname, 'cities.json'), json)
+                    res.send(json)
+                }
+            }
+        })
+    } else {
+        console.log('File do not exists')
+        res.status(404).send('No file');
+    }
+})
+
+app.listen(port, () => console.log("ok"));
